fdfc6507161c0530b1f9a80eba1e193d2a0b8a41
Roby Rafael <0341muzakky@gmail.com>
2024-12-07 20:18:25 +0700

    Update enemy chase range and idle movement in ArwahLogic.cs

--- Assets/Script/Player/WeaponLogic.cs
+++ Assets/Script/Player/WeaponLogic.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class WeaponLogic : MonoBehaviour
 {
     [SerializeField] private Transform AIMViewPoint;    // Titik pandang kamera (AIMViewPoint)
-    [SerializeField] private float range = 1000f;       // Jarak serangan
+    [SerializeField] private float range = 10f;       // Jarak serangan
     [SerializeField] private int photoDamage = 50;      // Damage yang diberikan saat memotret
 
     private bool isInCameraMode = false;
